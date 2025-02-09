a038421535f2b23ac304ff534a6df5cf38145c37
SujalR7 <sujalrandive7@gmail.com>
2025-02-10 00:57:50 +0530

    Update MainSection.jsx

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -3,14 +3,14 @@ import './App.css';
 import { useState } from 'react';
 export const MainSection=()=>{
     const [tasks, setTasks] = useState([
-        { id: 1,  description: "Todos", path:'/Todo',img:'./public/todo.jpg'},
-        { id: 2, description: "Notes",path:'/Notes',img:'./public/notes.png'},
-        { id: 3, description: "Calculator",path:'/Calculator',img:'./public/calculator.png'},
-        { id: 4,description: "Weather",path:'/Weather',img:'./public/weather.jpg'},
-        { id: 5,  description: "News",path:'/News',img:'./public/news.png'},
-        { id: 6, description: "Currency Converter",path:'/CurrencyConverter',img:'./public/converter.jpg'},
-        { id: 7, description: "Memory Game",path:'/MemoryGame',img:'./public/brain.png'},
-        { id: 8, description: "Cows And Bulls",path:'/CowsandBulls',img:'./public/Cb.jpg'},
+        { id: 1,  description: "Todos", path:'/Todo',img:'./todo.jpg'},
+        { id: 2, description: "Notes",path:'/Notes',img:'./notes.png'},
+        { id: 3, description: "Calculator",path:'/Calculator',img:'./calculator.png'},
+        { id: 4,description: "Weather",path:'/Weather',img:'./weather.jpg'},
+        { id: 5,  description: "News",path:'/News',img:'./news.png'},
+        { id: 6, description: "Currency Converter",path:'/CurrencyConverter',img:'./converter.jpg'},
+        { id: 7, description: "Memory Game",path:'/MemoryGame',img:'./brain.png'},
+        { id: 8, description: "Cows And Bulls",path:'/CowsandBulls',img:'./Cb.jpg'},
       ]);
     return<>
 <main>
